b6604fbf9e4bd4e4a9617e9b9590bbd973dee750
Suzie K <suziekim.dev@gmail.com>
2021-06-15 11:39:12 -0400

    Add active_storage

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,3 +1,3 @@
 class Admin::ProjectsController < ApplicationController
-
+  before_action :authenticate_user, expect: {:show, :index}
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord 
-  validates :name, presence: true 
-  validates :type, presence: true 
-  validates :description, presence: true 
-  validates :language, presence: true 
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :description, presence: true
+  validates :language, presence: true
+
+  has_one_attached :photo
 end
